2db2973453fb470ce0ca7035f070fae815512101
cmoore1711 <cmoore1711@gmail.com>
2017-10-30 04:15:08 -0400

    Add files via upload

---
+++ LYNX_UtilizationV1/LYNX_UtilizationV1/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace LYNX_UtilizationV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.excellentBtn = new System.Windows.Forms.Button();
+            this.poorBtn = new System.Windows.Forms.Button();
+            this.avgBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.qPromptLbl = new System.Windows.Forms.Label();
+            this.startOverBtn = new System.Windows.Forms.Button();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.testConnBtn = new System.Windows.Forms.Button();
+            this.previewSubBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // excellentBtn
+            // 
+            this.excellentBtn.AllowDrop = true;
+            this.excellentBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.excellentBtn.Font = new System.Drawing.Font("Georgia", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.excellentBtn.ForeColor = System.Drawing.Color.Snow;
+            this.excellentBtn.Location = new System.Drawing.Point(6, 164);
+            this.excellentBtn.Name = "excellentBtn";
+            this.excellentBtn.Size = new System.Drawing.Size(258, 150);
+            this.excellentBtn.TabIndex = 0;
+            this.excellentBtn.Text = "Excellent";
+            this.excellentBtn.UseVisualStyleBackColor = false;
+            this.excellentBtn.Click += new System.EventHandler(this.excellentBtn_Click);
+            // 
+            // poorBtn
+            // 
+            this.poorBtn.BackColor = System.Drawing.Color.Red;
+            this.poorBtn.Font = new System.Drawing.Font("Georgia", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.poorBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.poorBtn.Location = new System.Drawing.Point(534, 167);
+            this.poorBtn.Name = "poorBtn";
+            this.poorBtn.Size = new System.Drawing.Size(244, 150);
+            this.poorBtn.TabIndex = 1;
+            this.poorBtn.Text = "Poor";
+            this.poorBtn.UseVisualStyleBackColor = false;
+            this.poorBtn.Click += new System.EventHandler(this.poorBtn_Click);
+            // 
+            // avgBtn
+            // 
+            this.avgBtn.BackColor = System.Drawing.Color.Gold;
+            this.avgBtn.Font = new System.Drawing.Font("Georgia", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.avgBtn.ForeColor = System.Drawing.Color.Snow;
+            this.avgBtn.Location = new System.Drawing.Point(270, 164);
+            this.avgBtn.Name = "avgBtn";
+            this.avgBtn.Size = new System.Drawing.Size(258, 150);
+            this.avgBtn.TabIndex = 2;
+            this.avgBtn.Text = "Average";
+            this.avgBtn.UseVisualStyleBackColor = false;
+            this.avgBtn.Click += new System.EventHandler(this.avgBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(165, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(439, 43);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Utilization Data Entry";
+            // 
+            // qPromptLbl
+            // 
+            this.qPromptLbl.AutoSize = true;
+            this.qPromptLbl.Font = new System.Drawing.Font("Georgia", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.qPromptLbl.Location = new System.Drawing.Point(320, 95);
+            this.qPromptLbl.Name = "qPromptLbl";
+            this.qPromptLbl.Size = new System.Drawing.Size(0, 34);
+            this.qPromptLbl.TabIndex = 5;
+            // 
+            // startOverBtn
+            // 
+            this.startOverBtn.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startOverBtn.Location = new System.Drawing.Point(12, 323);
+            this.startOverBtn.Name = "startOverBtn";
+            this.startOverBtn.Size = new System.Drawing.Size(195, 92);
+            this.startOverBtn.TabIndex = 6;
+            this.startOverBtn.Text = "Start Over";
+            this.startOverBtn.UseVisualStyleBackColor = true;
+            this.startOverBtn.Click += new System.EventHandler(this.startOverBtn_Click);
+            // 
+            // startBtn
+            // 
+            this.startBtn.Font = new System.Drawing.Font("Georgia", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startBtn.Location = new System.Drawing.Point(6, 147);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(772, 170);
+            this.startBtn.TabIndex = 7;
+            this.startBtn.Text = "Begin";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.Font = new System.Drawing.Font("Georgia", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitBtn.Location = new System.Drawing.Point(6, 95);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(772, 222);
+            this.submitBtn.TabIndex = 8;
+            this.submitBtn.Text = "Submit to Database";
+            this.submitBtn.UseVisualStyleBackColor = true;
+            this.submitBtn.Click += new System.EventHandler(this.submitBtn_Click);
+            // 
+            // testConnBtn
+            // 
+            this.testConnBtn.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.testConnBtn.Location = new System.Drawing.Point(222, 323);
+            this.testConnBtn.Name = "testConnBtn";
+            this.testConnBtn.Size = new System.Drawing.Size(301, 92);
+            this.testConnBtn.TabIndex = 9;
+            this.testConnBtn.Text = "Test DB Connection";
+            this.testConnBtn.UseVisualStyleBackColor = true;
+            this.testConnBtn.Click += new System.EventHandler(this.testConnBtn_Click);
+            // 
+            // previewSubBtn
+            // 
+            this.previewSubBtn.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.previewSubBtn.Location = new System.Drawing.Point(539, 323);
+            this.previewSubBtn.Name = "previewSubBtn";
+            this.previewSubBtn.Size = new System.Drawing.Size(223, 92);
+            this.previewSubBtn.TabIndex = 10;
+            this.previewSubBtn.Text = "Preview Submission";
+            this.previewSubBtn.UseVisualStyleBackColor = true;
+            this.previewSubBtn.Click += new System.EventHandler(this.previewSubBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(330, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 34);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "N704";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 422);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.previewSubBtn);
+            this.Controls.Add(this.testConnBtn);
+            this.Controls.Add(this.submitBtn);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.startOverBtn);
+            this.Controls.Add(this.qPromptLbl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.avgBtn);
+            this.Controls.Add(this.poorBtn);
+            this.Controls.Add(this.excellentBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "LYNX_Utilization";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button excellentBtn;
+        private System.Windows.Forms.Button poorBtn;
+        private System.Windows.Forms.Button avgBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label qPromptLbl;
+        private System.Windows.Forms.Button startOverBtn;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button submitBtn;
+        private System.Windows.Forms.Button testConnBtn;
+        private System.Windows.Forms.Button previewSubBtn;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ LYNX_UtilizationV1/LYNX_UtilizationV1/Form1.cs
@@ -0,0 +1,224 @@
+﻿using MySql.Data.MySqlClient;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LYNX_UtilizationV1
+{
+    public partial class Form1 : Form
+    {
+        List<String> utilPrompts = new List<string>();
+        List<String> responses = new List<string>();
+
+
+        MySqlConnection connection;
+        string server = "DESKTOP-C42320E";
+        string database = "diamond_turning";
+        string currentTable = "utilization_704";
+        string uid = "root";
+        string password = "diamondturning";
+        string connectionString;
+        int clickCount = 0;
+        int numPrompts = 0;
+        string[] prompts = { "Form", "Finish", "Tool Lines", "Surface", "Metrics", "Etc" };
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+
+
+            foreach (string p in prompts)
+            {
+                utilPrompts.Add(p);
+            }
+
+
+            //utilPrompts.Add("Form");
+            //utilPrompts.Add("Finish");
+            //utilPrompts.Add("Tool lines");
+
+            submitBtn.Visible = false;
+            submitBtn.Enabled = false;
+            numPrompts = utilPrompts.Count;
+            qPromptLbl.Text = utilPrompts[clickCount];
+        }
+        private void excellentBtn_Click(object sender, EventArgs e)
+        {
+            responses.Add("1");
+            buttonClicked();
+
+        }
+        private void avgBtn_Click(object sender, EventArgs e)
+        {
+            responses.Add("0");
+            buttonClicked();
+        }
+
+        private void poorBtn_Click(object sender, EventArgs e)
+        {
+            responses.Add("-1");
+            buttonClicked();
+        }
+
+        void buttonClicked()
+        {
+            clickCount++;
+            if (clickCount < numPrompts)
+            {
+                qPromptLbl.Text = utilPrompts[clickCount];
+
+
+            }
+            if (clickCount == numPrompts)
+            {
+                qPromptLbl.Text = "Finished";
+                submitBtn.Enabled = true;
+                submitBtn.Visible = true;
+            }
+
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            qPromptLbl.Text = utilPrompts[clickCount];
+            startBtn.Enabled = false;
+            startBtn.Visible = false;
+
+        }
+
+        void pushToMySQL()
+        {
+
+            string entryListString = "1,1,-1";
+            string insertString = "INSERT INTO `diamond_turning`.`utilization_704`(`datetime`,`" +
+                "Operator`,`Form`,`Finish`,`Tool Lines`,`Utilx`,`Utily`,`Utilz`,`StartStop`)" +
+                " VALUES('NOW(),ColinTest," + entryListString + ",<{ Utilx: }>,<{ Utily: }>,<{ Utilz: }>,<{ StartStop: }>);";
+
+
+            MySqlConnection connection = null;
+            try
+            {
+                connection = new MySqlConnection(connectionString);
+                connection.Open();
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandText = insertString;
+                cmd.Prepare();
+
+                // cmd.Parameters.AddWithValue("@Name", "Bill Gates");
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                // MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
+            //using (MySqlConnection con = new MySqlConnection(connectionString))
+            //{
+            //    con.Open();
+
+            //    // Set up a command with the given query and associate
+            //    // this with the current connection.
+            //    using (MySqlCommand cmd = new MySqlCommand(insertString, con))
+            //    {
+            //        cmd.ExecuteNonQuery();
+
+            //    }
+
+            //}
+
+        }
+
+        private void startOverBtn_Click(object sender, EventArgs e)
+        {
+            clickCount = 0;
+            responses.Clear();
+            submitBtn.Enabled = false;
+            submitBtn.Visible = false;
+            qPromptLbl.Text = utilPrompts[clickCount];
+            // pushToMySQL();
+        }
+
+        private void submitBtn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Submitted.");
+        }
+
+        private void testConnBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection = new MySqlConnection(connectionString);
+                connection.Open();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
+        }
+
+        private void previewSubBtn_Click(object sender, EventArgs e)
+        {
+            string previewTxt = "Your entries so far: " +Environment.NewLine+ Environment.NewLine;
+            List<string> verboseResponse = new List<string>();
+
+            foreach (string s in responses)
+            {
+                switch (int.Parse(s))
+                {
+                    case 1:
+                        verboseResponse.Add("Good");
+                        break;
+                    case 0:
+                        verboseResponse.Add("Average");
+                        break;
+
+                    case -1:
+                        verboseResponse.Add("Bad");
+                        break;
+
+                }
+
+
+            }
+
+            int smaller = Math.Min(verboseResponse.Count(), prompts.Count());
+            for (int i = 0; i < smaller; i++)
+            {
+                previewTxt += prompts[i] + ": " + verboseResponse[i] + Environment.NewLine+Environment.NewLine;
+            }
+            MessageBox.Show(previewTxt);
+        }
+        //INSERT INTO `diamond_turning`.`utilization_704`(`datetime`,`Operator`,`Form`,`Finish`,`Tool Lines`,`Utilx`,`Utily`,`Utilz`,`StartStop`)
+        //VALUES(<{ datetime: }>,<{Operator: }>,<{Form: }>,<{Finish: }>,<{Tool Lines: }>,<{Utilx: }>,<{Utily: }>,<{Utilz: }>,<{StartStop: }>);
+    }
+}
+
+
+
